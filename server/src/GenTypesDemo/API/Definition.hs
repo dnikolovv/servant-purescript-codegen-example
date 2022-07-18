795e5230f72a3f34f193c914054b6814dd7f70dd
Dobromir Nikolov <dnikolovv@hotmail.com>
2022-07-18 17:37:34 +0300

    Use effects for application logic instead of mtl.

--- server/src/GenTypesDemo/API/Definition.hs
+++ server/src/GenTypesDemo/API/Definition.hs
@@ -1,23 +1,23 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 
 module GenTypesDemo.API.Definition where
 
-import Control.Monad.Error.Class
-import qualified Data.Aeson as JSON
-import Data.Generics.Product (HasType (typed))
-import Data.List (sortOn)
-import Data.UUID.V4 (nextRandom)
+import Effectful (Eff)
+import qualified Effectful as E
+import qualified Effectful.Error.Static as ES
 import GenTypesDemo.API.Auth (APIUser)
+import GenTypesDemo.API.ManageUsers (ManageUsers, deleteUser, getAllUsers, getUser, newUser, updateUser)
 import GenTypesDemo.API.Types
 import RIO
-import qualified RIO.HashMap as HM
-import RIO.Time (getCurrentTime)
 import Servant
 import Servant.Auth.Server
 
@@ -35,7 +35,7 @@ type PublicAPI =
     :<|> ( "user"
              :> ( Capture "userId" UserId :> Get '[JSON] User
                     :<|> ReqBody '[JSON] CreateUserRequest :> Post '[JSON] User
-                    :<|> Capture "userId" UserId :> ReqBody '[JSON] UpdateUserRequest :> PutNoContent
+                    :<|> Capture "userId" UserId :> ReqBody '[JSON] UpdateUserRequest :> Put '[JSON] ()
                 )
          )
 
@@ -45,17 +45,10 @@ type ProtectedAPI =
 
 type UsersTable = IORef (HashMap UserId UserData)
 
-newtype AppM a = AppM {runAppM :: ReaderT UsersTable Servant.Handler a}
-  deriving newtype
-    ( Functor,
-      Applicative,
-      Monad,
-      MonadIO,
-      MonadReader UsersTable,
-      MonadError ServerError
-    )
-
-server :: ServerT UsersAPI AppM
+server ::
+  ManageUsers E.:> es =>
+  ES.Error ServerError E.:> es =>
+  ServerT UsersAPI (Eff es)
 server =
   publicServer
     :<|> protectedServer
@@ -63,69 +56,10 @@ server =
     publicServer =
       getAllUsers
         :<|> getUser
-        :<|> createUser
-        :<|> updateUser
+        :<|> (\CreateUserRequest {..} -> newUser email username)
+        :<|> (\uId UpdateUserRequest {..} -> updateUser uId newEmail newUsername)
 
-    protectedServer (Authenticated _) uId =
+    protectedServer (Authenticated _) uId = do
       deleteUser uId
     protectedServer _ _ =
-      throwError $ servantErrorWithText err401 "Unauthorized."
-
-    getAllUsers =
-      sortOn (created . info) . map (uncurry User) . HM.toList <$> (ask >>= readIORef)
-    getUser uId = do
-      userMay <- lookupUser uId
-      case userMay of
-        Just u -> pure $ User uId u
-        Nothing ->
-          throwError $
-            servantErrorWithText err404 "User not found."
-    createUser CreateUserRequest {..} = do
-      newUserId <- UserId <$> liftIO nextRandom
-      now <- CreatedAt <$> getCurrentTime
-      let userData = UserData email username now
-      usersRef <- ask
-      modifyIORef' usersRef $ HM.insert newUserId userData
-      getUser newUserId
-    updateUser uId UpdateUserRequest {..} = do
-      validate (isJust <$> lookupUser uId) $
-        servantErrorWithText err400 "Unexisting user."
-
-      usersRef <- ask
-      modifyIORef' usersRef $
-        HM.adjust
-          ( \userData ->
-              userData
-                & typed @Email %~ (`fromMaybe` newEmail)
-                & typed @Username %~ (`fromMaybe` newUsername)
-          )
-          uId
-
-      pure NoContent
-    deleteUser uId =
-      ask >>= \usersRef -> modifyIORef' usersRef (HM.delete uId) >> pure ()
-
-    lookupUser :: UserId -> AppM (Maybe UserData)
-    lookupUser uId = ask >>= readIORef >>= pure . HM.lookup uId
-
-    validate :: AppM Bool -> ServerError -> AppM ()
-    validate condition err =
-      condition >>= \result ->
-        if result
-          then pure ()
-          else throwError err
-
-    servantErrorWithText ::
-      Servant.ServerError ->
-      Text ->
-      Servant.ServerError
-    servantErrorWithText sErr msg =
-      sErr
-        { errBody = errorBody (errHTTPCode sErr),
-          errHeaders = [jsonHeaders]
-        }
-      where
-        errorBody code = JSON.encode $ Error msg code
-
-    jsonHeaders =
-      (fromString "Content-Type", "application/json;charset=utf-8")
+      ES.throwError err401
